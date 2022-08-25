2030e06696b34a04e4c772ee934417a7d60ead2f
TH <THIERRy.hentic@gmail.com>
2022-08-25 21:21:47 +0200

    Initial Commit

---
+++ BergeracWoCharts/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BergeracWoCharts
+//
+//  Created by thierry hentic on 25/08/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
